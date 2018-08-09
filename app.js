13c3c6f4d30a56f1e5e5a12386fc4287c9a4d3ab
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-08-09 18:03:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,62 @@ app.use(bodyParser.json());
 
     console.log("tester");
   res.send({
-    speech:"Please enter a valid incident number",
-    displayText:"Please enter a valid incident number"
-  });
+  "platform": "google",
+  "type": "custom_payload",
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "isSsml": false,
+      "noInputPrompts": [],
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "displayText": "hi",
+              "textToSpeech": "hello"
+            }
+          }
+        ],
+        "suggestions": [
+          {
+            "title": "Say this"
+          },
+          {
+            "title": "or this"
+          }
+        ]
+      },
+      "systemIntent": {
+        "data": {
+          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+          "listSelect": {
+            "items": [
+              {
+                "optionInfo": {
+                  "key": "key1",
+                  "synonyms": [
+                    "key one"
+                  ]
+                },
+                "title": "must not be empty"
+              },
+              {
+                "optionInfo": {
+                  "key": "key2",
+                  "synonyms": [
+                    "key two"
+                  ]
+                },
+                "title": "must not be empty, but unique, for some reason"
+              }
+            ]
+          }
+        },
+        "intent": "actions.intent.OPTION"
+      }
+    }
+  }
+});
 
 
 
