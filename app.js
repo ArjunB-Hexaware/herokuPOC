ec17bc6eb4c2c9ae9b5dc44cef4ce3f1719882b8
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-08-09 17:27:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,32 +21,20 @@ var options = {
    app.post('/',function(req,res){
       console.log('REQUESTTTTT:::'+JSON.stringify(req.body));
 if(req.body.queryResult.intent.displayName=='LeaveBalance'){
-  res.json({
-  "platform": "google",
-  "type": "custom_payload",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "isSsml": false,
-      "noInputPrompts": [],
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "displayText": "hi",
-              "textToSpeech": "hello"
-            }
-          }
-        ],
-        "suggestions": [
-          {
-            "title": "Say this"
-          },
-          {
-            "title": "or this"
-          }
-        ]
-      });
+  res.json({"messages": [
+  {
+    "platform": "google",
+    "suggestions": [
+      {
+        "title": "Chip One"
+      },
+      {
+        "title": "Chip Two"
+      }
+    ],
+    "type": "suggestion_chips"
+  }
+]});
 
 
 
