db8b260aa3d5627edd41ec97d278b837c96a6b97
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-08-09 18:13:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,43 @@ app.use(bodyParser.json());
     "platform": "google",
     "textToSpeech": "Audio response",
     "type": "simple_response"
+  },
+     {
+    "items": [
+      {
+        "description": "Item One Description",
+        "image": {
+          "url": "http://imageOneUrl.com"
+          "accessibilityText": "Image description for screen readers"
+        },
+        "optionInfo": {
+          "key": "itemOne",
+          "synonyms": [
+            "thing one",
+            "object one"
+          ]
+        },
+        "title": "Item One"
+      },
+      {
+        "description": "Item Two Description",
+        "image": {
+          "url": "http://imageTwoUrl.com"
+          "accessibilityText": "Image description for screen readers"
+        },
+        "optionInfo": {
+          "key": "itemTwo",
+          "synonyms": [
+            "thing two",
+            "object two"
+          ]
+        },
+        "title": "Item Two"
+      }
+    ],
+    "platform": "google",
+    "title": "Title",
+    "type": "list_card"
   }
 ]});
 
