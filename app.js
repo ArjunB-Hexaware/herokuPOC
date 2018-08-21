b88c703e012f3f3141383e91aa041fe62a6a1610
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-08-21 10:56:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,8 @@ app.use(bodyParser.json());
        
   ]});
       }else{
-         
+               console.log("userStorage", JSON.stringify(req.body));
+         console.log("userstorageValue",req.body.originalRequest.data.user.userStorage)
           if(req.body.result.fulfillment.speech == "success"){
             res.send({"messages": [
                 {
