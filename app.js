911f3f63c55a4e2e34d6056745ccc63079bbaa52
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-09-06 17:07:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -98,86 +98,60 @@ app.use(bodyParser.json());
 });*/
          
          res.send({
-    "conversationToken": "",
-    "expectUserResponse": true,
-        "userStorage": "{'test':'test2'}",
-    "expectedInputs": [
-        {
-            "inputPrompt": {
-                "richInitialPrompt": {
-                "items": [
+  "conversationToken": "",
+  "expectUserResponse": true,
+  "expectedInputs": [
+    {
+      "inputPrompt": {
+        "richInitialPrompt": {
+          "items": [
             {
               "simpleResponse": {
                 "textToSpeech": "List"
               }
             }
-          ]
-                }
+          ],
+          "suggestions": [
+            {
+              "title": "Raj"
             },
-            "possibleIntents": [
-                {
-                    "intent": "actions.intent.OPTION",
-                    "inputValueData": {
-                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                        "listSelect": {
-                            "title": "Things to learn about",
-                            "items": [
-                                {
-                                    "optionInfo": {
-                                        "key": "MATH_AND_PRIME",
-                                        "synonyms": [
-                                            "math",
-                                            "math and prime",
-                                            "prime numbers",
-                                            "prime"
-                                        ]
-                                    },
-                                    "title": "Math & prime numbers",
-                                    "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                                    "image": {
-                                        "url": "http://example.com/math_and_prime.jpg",
-                                        "accessibilityText": "Math & prime numbers"
-                                    }
-                                },
-                                {
-                                    "optionInfo": {
-                                        "key": "EGYPT",
-                                        "synonyms": [
-                                            "religion",
-                                            "egpyt",
-                                            "ancient egyptian"
-                                        ]
-                                    },
-                                    "title": "Ancient Egyptian religion",
-                                    "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                                    "image": {
-                                        "url": "http://example.com/egypt",
-                                        "accessibilityText": "Egypt"
-                                    }
-                                },
-                                {
-                                    "optionInfo": {
-                                        "key": "RECIPES",
-                                        "synonyms": [
-                                            "recipes",
-                                            "recipe",
-                                            "42 recipes"
-                                        ]
-                                    },
-                                    "title": "42 recipes with 42 ingredients",
-                                    "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                                    "image": {
-                                        "url": "http://example.com/recipe",
-                                        "accessibilityText": "Recipe"
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
+            {
+              "title": "arj"
+            }
+          ]
         }
-    ]
+      },
+      "possibleIntents": [
+        {
+          "intent": "actions.intent.OPTION",
+          "inputValueData": {
+            "listSelect": {
+              "title": "123",
+              "items": [
+                {
+                  "optionInfo": {
+                    "key": "rajesh",
+                    "synonyms": []
+                  },
+                  "title": "Rajesh"
+                },
+                {
+                  "optionInfo": {
+                    "key": "Arjun",
+                    "synonyms": []
+                  },
+                  "title": "Arjun",
+                  "description": ""
+                }
+              ]
+            },
+            "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
+          }
+        }
+      ]
+    }
+  ],
+  "userStorage": "{'name':'rajesh'}"
 });
         
 
