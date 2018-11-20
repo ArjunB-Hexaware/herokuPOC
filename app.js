c93ec45eda9c04c53b188205562f8850802313ec
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-11-20 18:32:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,107 +9,78 @@ var portC = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.get('/getLeaveBalance',function(req,res){
+app.get('/TICKETCOUNT',function(req,res){
     
 res.send({
-    "HX_ES_BOT_LV_BAL_RES": {
+    "SERVICE_TKTS_RES": {
         "EMPLID": "00000",
-        "HX_ES_BOT_LBAL_CHD_DOC": [
-            {
-                "LEAVE_TYPE": "EL",
-                "LEAVE_DESCR": "Earned Leave",
-                "LEAVEBALANCE": "4",
-                "ACTUAL_BALANCE": "1 Available, 3 Pending"
-            },
-            {
-                "LEAVE_TYPE": "LOP",
-                "LEAVE_DESCR": "Loss of Pay",
-                "LEAVEBALANCE": "-1",
-                "ACTUAL_BALANCE": "-1 Available, 0 Pending"
-            },
-            {
-                "LEAVE_TYPE": "OPH",
-                "LEAVE_DESCR": "Optional Holiday",
-                "LEAVEBALANCE": "1",
-                "ACTUAL_BALANCE": "0 Available, 1 Pending"
-            },
-            {
-                "LEAVE_TYPE": "PATL",
-                "LEAVE_DESCR": "Paternity Leave",
-                "LEAVEBALANCE": "3",
-                "ACTUAL_BALANCE": "3 Available, 0 Pending"
-            }
-        ]
+        "SERVICE_TKT_COUNTS": [
+  {
+    "TICKET_NAME": "INTERNET",
+    "TOTAL_COUNT": "11",
+    "PRIORITY_COUNT": {
+      "HIGH": "2",
+      "MEDIUM": "5",
+      "LOW": "3"
+    }
+  },
+  {
+    "TICKET_NAME": "ACCOUNT LOCKED",
+    "TOTAL_COUNT": "8",
+    "PRIORITY_COUNT": {
+      "HIGH": "8",
+      "MEDIUM": "0",
+      "LOW": "0"
+    }
+  },
+  {
+    "TICKET_NAME": "ADMIN ACCESS",
+    "TOTAL_COUNT": "5",
+    "PRIORITY_COUNT": {
+      "HIGH": "0",
+      "MEDIUM": "1",
+      "LOW": "4"
+    }
+  },
+  {
+    "TICKET_NAME": "HARDWARE",
+    "TOTAL_COUNT": "6",
+    "PRIORITY_COUNT": {
+      "HIGH": "1",
+      "MEDIUM": "3",
+      "LOW": "2"
+    }
+  }
+]
     }
 }).end();
 });
 
-app.post('/getOptionalHolidays',function(req,res){
+app.post('/REQUESTSTATUS',function(req,res){
 res.send({
-    "Optional Holiday List": {
-        "EMPLID": "00000",
-        "COUNTRY": "IND",
-        "HX_ES_BOT_OPHL_RSD": [
+    "ID CARD REQUESTS": {
+        "CARD_REQUESTS": [
             {
-                "HOLIDAY_DATE": "2018-12-25",
-                "HOLIDAY_NAME": "Christmas",
-                "LEAVE_STATUS": "Pending",
-                "FLAG": "Y"
+                "NAME": "RAJESH",
+                "DESIGNATION": "SR. SOFTWARE ENGINEER",
+                "STATUS": "Pending"
             },
             {
-                "HOLIDAY_DATE": "2018-12-12",
-                "HOLIDAY_NAME": "OPH Alpha Testing 5",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
+                "HOLIDAY_DATE": "RAHUL",
+                "HOLIDAY_NAME": "VICE PRESIDENT",
+                "STATUS": "Dispatched"
             },
             {
-                "HOLIDAY_DATE": "2018-11-30",
-                "HOLIDAY_NAME": "OPH Alpha Testing 4",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
+                "HOLIDAY_DATE": "Krishna",
+                "HOLIDAY_NAME": "TECHNICAL ARCHITECT",
+                "LEAVE_STATUS": "Closed"
             },
             {
-                "HOLIDAY_DATE": "2018-11-22",
-                "HOLIDAY_NAME": "OPH Alpha Testing 4",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
+                "HOLIDAY_DATE": "Ram",
+                "HOLIDAY_NAME": "SENIOR CONSULTANT",
+                "LEAVE_STATUS": "Dispatched"
             },
-            {
-                "HOLIDAY_DATE": "2018-11-15",
-                "HOLIDAY_NAME": "OPH Alpha Testing 3",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
-            },
-            {
-                "HOLIDAY_DATE": "2018-10-25",
-                "HOLIDAY_NAME": "OPH Alpha Testing 2",
-                "LEAVE_STATUS": "Approved",
-                "FLAG": "N"
-            },
-            {
-                "HOLIDAY_DATE": "2018-10-05",
-                "HOLIDAY_NAME": "OPH Alpha Testing 1",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
-            },
-            {
-                "HOLIDAY_DATE": "2018-09-13",
-                "HOLIDAY_NAME": "Ganesh Chaturthi",
-                "LEAVE_STATUS": "Cancelled",
-                "FLAG": "N"
-            },
-            {
-                "HOLIDAY_DATE": "2018-06-15",
-                "HOLIDAY_NAME": "Id-Ul_fitr",
-                "LEAVE_STATUS": "Approved",
-                "FLAG": "Y"
-            },
-            {
-                "HOLIDAY_DATE": "2018-03-30",
-                "HOLIDAY_NAME": "Good Friday",
-                "LEAVE_STATUS": "",
-                "FLAG": "N"
-            }
+            
         ]
     }
 }).end();
